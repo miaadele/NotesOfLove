fix normalization
fix normalization so stopwords are removed from lyrics even if punctuation is attached

re-run NER files to accommodate for changes in the cleaned lyrics

NER/graph_cooccurrences.py
@@ -1,21 +1,45 @@
 import json
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
+from statsmodels.nonparametric.smoothers_lowess import lowess
 
 with open("time_date_cooccurrences.json", "r", encoding = "utf-8") as f:
     data = json.load(f)
 
 df = pd.DataFrame(data)
 
-#count by year
-year_counts = df["Year"].value_counts().sort_index()
+#Split df into DATE and TIME
+df_date = df[df["entity_label"] == "DATE"]
+df_time = df[df["entity_label"] == "TIME"]
 
-#plot date and time
-grouped = df.groupby(["Year", "entity_label"]).size().unstack(fill_value=0)
+#Count per year
+date_cts = df_date["Year"].value_counts().sort_index()
+time_cts = df_time["Year"].value_counts().sort_index()
 
-grouped.plot()
+#Align years
+all_yrs = sorted(set(date_cts.index).union(set(time_cts.index)))
+
+date_y = np.array([date_cts.get(y,0) for y in all_yrs])
+time_y = np.array([time_cts.get(y,0) for y in all_yrs])
+years = np.array(all_yrs)
+
+#LOWESS smoothing
+date_smooth = lowess(date_y, years, frac = 0.1)
+time_smooth = lowess(time_y, years, frac = 0.1)
+
+plt.figure()
+
+#Raw points
+plt.scatter(years, date_y, label = "DATE (raw)", alpha = 0.6, color = "#B81456")
+plt.scatter(years, time_y, label = "TIME (raw)", alpha = 0.6, color = "#FF99C2")
+
+#LOWESS smoothed lines
+plt.plot(date_smooth[:, 0], date_smooth[:, 1], linestyle='--', label="DATE Trend", color = "#B81456")
+plt.plot(time_smooth[:, 0], time_smooth[:, 1], linestyle='--', label="TIME Trend", color = "#FF99C2")
 
 plt.xlabel("Year that Song is On Top 100 List")
 plt.ylabel("Count of DATE/TIME entities")
 plt.title("DATE/TIME Entities Over Time, Coocurring with the Word 'Love'")
-plt.show()
+plt.legend()
+plt.show() 

year_plot.py
@@ -17,9 +17,20 @@ for entry in data:
 years = sorted(year_counts.keys())
 counts = [year_counts[y] for y in years]
 
+#make scatterplot of love occurrences in corpus
 import matplotlib.pyplot as plt
+import numpy as np
+
+years = np.array(years)
+counts = np.array(counts)
+from statsmodels.nonparametric.smoothers_lowess import lowess
+smoothed = lowess(counts, years, frac = 0.2) #for each point, LOWESS looks at 20% of nearby data points to fit a small regression
 plt.figure(figsize= (10, 6))
-plt.plot(years, counts, marker = 'o')
+plt.scatter(years, counts, color = "#B81456")
+plt.plot(smoothed[:, 0], smoothed[:, 1], linestyle='--', color = "#B81456")
+
 
 plt.title("Occurrences of the Word 'Love' in US Top 100 Song Lyrics From 1959 Through 2023")
-plt.savefig("love_occurerences_dist.png")
+plt.xlabel("Year of Song Ranking")
+plt.ylabel("Occrrences of the Word 'Love'")
+plt.savefig("love_occurrences_lowess_smoothing.png")